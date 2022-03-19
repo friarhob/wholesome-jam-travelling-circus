d7a4f6892094f371671307b4fcb1778fd4caca43
friarhob <github@friarhob.33mail.com>
2022-03-19 14:45:30 +0000

    Fixing typo on dialogue name

---
+++ Assets/Scripts/Managers/DIaloguesManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class DialoguesManager : MonoBehaviour
+{
+
+    public event UnityAction OnDialogStarted;
+    public event UnityAction OnDialogFinished;
+
+    private Dialogue currentDialog;
+    List<Phrase> Phrases = new List<Phrase>();
+    int currentPhraseIndex;
+
+    [SerializeField]
+    AudioSource Dictor;
+    [SerializeField]
+    TextMeshProUGUI PhraseText;
+
+
+    void Start()
+    {
+        
+    }
+
+    public void StartDialog(Dialogue dialog)
+    {
+
+        currentPhraseIndex = 0;
+        currentDialog = dialog;
+        Phrases = currentDialog.GetPhrases();
+        Debug.Log("Dialog starts!");
+        OnDialogStarted?.Invoke();
+        StartCoroutine("PlayPhrase", currentPhraseIndex);
+    }
+
+    public void FinishDialog()
+    {
+        Debug.Log("Dialog finished!");
+        OnDialogFinished?.Invoke();
+    }
+  
+
+    IEnumerator PlayPhrase(int phraseNumber)
+    {
+
+        if (phraseNumber < Phrases.Count)
+        {
+            if (Phrases[currentPhraseIndex].Diction)
+            {
+                Dictor.clip = Phrases[currentPhraseIndex].Diction;
+                Dictor.Play();
+            }
+
+            PhraseText.text = Phrases[currentPhraseIndex].Text;
+            yield return new WaitForSeconds(Phrases[currentPhraseIndex].Time);
+            currentPhraseIndex++;
+            StartCoroutine("PlayPhrase", currentPhraseIndex);
+        }
+        else
+        {
+            
+            FinishDialog();
+        }
+
+
+    }
+
+}
