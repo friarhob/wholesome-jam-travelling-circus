b70cd6a70cb8007184ad5bc5e3fcbb493db77eaf
friarhob <github@friarhob.33mail.com>
2022-03-19 12:51:20 +0000

    Fixing typos and lack of capitalization

--- Assets/Scripts/Managers/DIalogsManager.cs
+++ Assets/Scripts/Managers/DIalogsManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DIalogsManager : MonoBehaviour
+public class DialogsManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
