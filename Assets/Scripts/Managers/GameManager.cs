56689f949ec6557770d453396e1351a94d6fcfff
friarhob <github@friarhob.33mail.com>
2022-03-19 09:56:17 +0000

    Scene transition with intro dialogues

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public SoundManager soundManager;
+    public GameObject mainMenuCanvas;
+    public GameObject[] introDialogueCanvases;
+    private int currentIntroDialogue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenuCanvas.SetActive(true);
+        currentIntroDialogue = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void StartGame()
+    {
+        mainMenuCanvas.SetActive(false);
+        Invoke("LoadIntroDialogue", 1f);
+    }
+
+    private void LoadIntroDialogue()
+    {
+        if(currentIntroDialogue > 0)
+        {
+            introDialogueCanvases[currentIntroDialogue-1].SetActive(false);
+        }
+
+        if(currentIntroDialogue < introDialogueCanvases.Length)
+        {
+            introDialogueCanvases[currentIntroDialogue].SetActive(true);
+            currentIntroDialogue++;
+            Invoke("LoadIntroDialogue", 4f);
+        }
+        else
+        {
+            SceneManager.LoadScene("GameScene");
+        }
         
     }
 }
