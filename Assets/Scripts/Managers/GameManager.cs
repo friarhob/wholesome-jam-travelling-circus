98c4fb9f63894781fc23a5e115fdabd62c02d4e5
Friar Hob <github@friarhob.33mail.com>
2022-03-20 13:49:39 +0000

    Organizing game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public SoundManager soundManager;
     public MenuManager menuManager;
     public DialogsManager dialogueManager;
+    public LevelManager levelManager;
 
     void Awake()
     {
@@ -20,57 +21,15 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-
-        mainMenuCanvas.SetActive(true);
-        currentIntroDialogue = 0;
-        levelRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (levelRunning)
-        {
-            remainingTime -= Time.deltaTime;
-            if (remainingTime < 0)
-            {
-                levelRunning = false;
-                // TODO: add night time;
-            }
-        }
     }
 
     public void StartGame()
     {
-        mainMenuCanvas.SetActive(false);
-        Invoke("LoadIntroDialogue", 1f);
-    }
-
-    private void LoadIntroDialogue()
-    {
-        if (currentIntroDialogue > 0)
-        {
-            introDialogueCanvases[currentIntroDialogue - 1].SetActive(false);
-        }
-
-        if (currentIntroDialogue < introDialogueCanvases.Length)
-        {
-            introDialogueCanvases[currentIntroDialogue].SetActive(true);
-            currentIntroDialogue++;
-            Invoke("LoadIntroDialogue", 4f);
-        }
-        else
-        {
-            RunGame();
-        }
-
-    }
-
-    private void RunGame()
-    {
         SceneManager.LoadScene("GameScene");
-        levelRunning = true;
-        level = 1;
-        remainingTime = 10f - level;
     }
 }
